75e7bc08f3bb6f0d45f7c5ef88e8e5187d23ab6f
woo <woo>
2016-11-29 22:34:34 +0900

    test commit

--- routes/posts.js
+++ routes/posts.js
@@ -96,6 +96,20 @@ router.put('/:id', function(req, res, next) {
   });
 });
 
+function needAuth(req, res, next) {
+  if (req.isAuthenticated()) {
+    next();
+  } else {
+    req.flash('danger', '로그인이 필요합니다.');
+    res.redirect('/signin');
+  }
+}
+
+router.get('/', needAuth, function(req, res, next) {
+  res.render('todos');
+});
+
+
 
 module.exports = router;
 
